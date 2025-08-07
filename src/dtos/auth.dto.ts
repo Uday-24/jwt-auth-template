ca19e254cae62fdc93ec3e8c86dcc87b0f4d13dc
Uday-24 <chotaliyauday24@gmail.com>
2025-08-07 09:47:46 +0530

    DTOs for auth created

---
+++ src/dtos/auth.dto.ts
@@ -0,0 +1,14 @@
+export interface RegisterDto {
+    fullname: string;
+    email: string;
+    password: string;
+}
+
+export interface LoginDto {
+    email: string;
+    password: string;
+}
+
+export interface TokenPayloadDto {
+    id: string;
+}
