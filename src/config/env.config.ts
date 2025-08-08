efc1c1e966c177c069e00a662f678f161282b252
Uday-24 <chotaliyauday24@gmail.com>
2025-08-08 15:55:16 +0530

    Manual expiration time

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,11 +1,10 @@
 import dotenv from "dotenv";
-
 dotenv.config();
 
 export const config = {
     port: process.env.PORT,
     jwtAccessSecret: process.env.JWT_ACCESS_SECRET,
     jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
-    accessTokenExpiry: process.env.ACCESS_TOKEN_EXPIRY,
-    refreshTokenExpiry: process.env.REFRESH_TOKEN_EXPIRY,
+    accessTokenExpiry: 900,
+    refreshTokenExpiry: 7 * 24 * 60 * 60,
 }
